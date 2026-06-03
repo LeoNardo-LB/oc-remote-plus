e2cf83b3256b9be2295dfc7e8869afd7e5baa055
leonardo <tab206388@aliyun.com>
2026-06-03 23:56:39 +0800

    release: v2.0.0-beta.154

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "dev.minios.ocremote"
         minSdk = 26
         targetSdk = 35
-        versionCode = 353
-        versionName = "2.0.0-beta.153"
+        versionCode = 354
+        versionName = "2.0.0-beta.154"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
